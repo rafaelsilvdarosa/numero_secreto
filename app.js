32c958ac37945ff6321110c5c37dea3d834a4fc6
Rafael_Rosa <rafaelsilvadarosa@gmail.com>
2024-07-01 18:29:15 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativasAcerto = 1;
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+exibirTextoNaTela("h1", "Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+
+function verificarChute() {
+  let chute = document.querySelector("input").value;
+
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou!");
+    let palavraTentativa = tentativasAcerto > 1 ? "tentativas" : "tentativa";
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativasAcerto} ${palavraTentativa}!`;
+    exibirTextoNaTela("p", mensagemTentativas);
+    document.getElementById("reiniciar").removeAttribute("disabled");
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "O número secreto é menor");
+    } else {
+      exibirTextoNaTela("p", "O número secreto é maior");
+    }
+    tentativasAcerto++;
+    limparCampo();
+  }
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+  chute = document.querySelector("input");
+  chute.value = "";
+}
